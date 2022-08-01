598cacc9eef8568cd1963ca3fff8bf9cae741c1d
Charlie Carter <charliec1665@gmail.com>
2022-07-31 23:26:23 -0400

    password with special and number

--- Assets/script.js
+++ Assets/script.js
@@ -114,6 +114,14 @@ var generatePassword = function () {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+  } else if (confirmSpecial && confirmNumber) {
+    var result = "";
+    var characters = "0123456789!#$%&*+-./:;<>=?@^_";
+    var charactersLength = characters.length;
+    for (i = 0; i < promptLength; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
   console.log(generatePassword(promptLength));
 
