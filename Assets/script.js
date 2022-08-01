16b49e17db69c48efcb9dc0d9e838509ff76a976
Charlie Carter <charliec1665@gmail.com>
2022-07-31 23:33:03 -0400

    reprompt if no characters confirmed

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,3 @@
-//create array for lowercase, uppercase, numeric, and special characters
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numeric = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var special = ['!', '#', '$', '%', '&', '*', '+', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '^', '_'];
 
 var generatePassword = function () {
   debugger;
@@ -154,7 +149,11 @@ var generatePassword = function () {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+  } else {
+    window.alert("Please choose at least one set of characters.");
+    return generatePassword();
   }
+
   console.log(generatePassword(promptLength));
 
 }
