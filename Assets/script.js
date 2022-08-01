5405943caed93e31ac1c1a19fc9fbaced830b4ca
Charlie Carter <charliec1665@gmail.com>
2022-07-31 20:36:09 -0400

    confirm lowercase and uppercase

--- Assets/script.js
+++ Assets/script.js
@@ -6,10 +6,17 @@ var special = ['!', '#', '$', '%', '&', '*', '+', '-', '.', '/', ':', ';', '<', 
 
 var generatePassword = function () {
   //prompt user for length of password (>8 <128) using a window.prompt
-  var promptLength = window.prompt("How long would you like your password to be? Must be longer than 8 characters.");
+  var promptLength = "";
+
+  while (promptLength === "" || promptLength === null) {
+    promptLength = prompt("How long would you like your password to be? Must be longer than 8 characters.");
+  }
 
   //conditional recursive function call
-  if (promptLength < '8') {
+  if (promptLength < 8) {
+    window.alert("Your password must be between 8 and 128 characters. Please try again.");
+    return generatePassword();
+  } else if (promptLength > 128) {
     window.alert("Your password must be between 8 and 128 characters. Please try again.");
     return generatePassword();
   }
@@ -17,8 +24,28 @@ var generatePassword = function () {
   //prompt user whether they would like to include lowercase characters
   var confirmLowercase = window.confirm("Would you like to include lowercase characters?");
 
+  var confirmUppercase = window.confirm("Would you like to include uppercase characters?");
+
   if (confirmLowercase) {
-    return true;
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuv'
+    var charactersLength = promptLength;
+    for ( var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  } else if (confirmLowercase && confirmUppercase) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv'
+    var charactersLength = promptLength;
+    for ( var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+  
+  else {
+    window.alert("Your password must include at least one type of character.");
   }
 
   //prompt user whether they would like to include uppercase characters
@@ -28,6 +55,8 @@ var generatePassword = function () {
   //prompt user whether they would like to include special characters
 
   //based on user selections, use randomization to go through their selected arrays and generate a password
+
+  console.log(generatePassword(promptLength));
 }
 
 
